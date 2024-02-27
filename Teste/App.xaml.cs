261a3e0a588674859c8716917f2134121e6ce4e0
Bruno Henrique <brunohmfcorporativo@gmail.com>
2024-02-26 21:19:42 -0300

    Feat: adicionado projeto teste

---
+++ Teste/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Teste
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Teste/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace Teste
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string name = "";
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+  
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void OnHelloClicked(object sender, EventArgs e)
+        {
+            name = NameInput.Text;
+            GreetingLabel.Text = $"Hello, {name}";
+
+            SemanticScreenReader.Announce(GreetingLabel.Text);
+
+        }
+    }
+
+}
